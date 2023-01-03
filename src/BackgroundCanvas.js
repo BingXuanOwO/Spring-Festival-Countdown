41345ebe4fb5cce2ca726b82f7ec2fae7d91b2cc
BingXuanOwO <hxc35459@outlook.com>
2023-01-03 13:12:28 +0800

    can gen firework when clicked

--- src/BackgroundCanvas.js
+++ src/BackgroundCanvas.js
@@ -1,17 +1,67 @@
 import React from "react";
 import './BackgroundCanvas.css'
 
+import { Firework } from "./functions";
+
+var Fireworks = []
+
+const drawTicks = ()=>{
+    // 清除画布
+    const canvas = document.querySelector("canvas");
+    if(canvas!=null){
+        const ctx = canvas.getContext("2d")
+        ctx.fillStyle = 'rgba(0,0,0,0.2)'
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+    }
+
+    // 绘制每个烟花
+    if( Fireworks != 0 ){
+        Fireworks.forEach((element,index)=>{
+            element.drawFireworks()
+            // console.log(element.alpha)
+            if(element.alpha < 0){
+                Fireworks.splice(index,1)
+                console.log(Fireworks)
+            }
+        })
+    }
+    requestAnimationFrame(drawTicks)
+}
+
+drawTicks()
 
 class BackgroundCanvas extends React.Component {
-    state = { 
-        
-    } 
+    constructor(props){
+        super(props)
+        window.onload = ()=>{
+            const canvas = document.querySelector("canvas");
+
+            // canvas自动更换高度
+            canvas.width = window.innerWidth
+            canvas.height = window.innerHeight
+            window.addEventListener("resize", ()=>{
+                canvas.width = window.innerWidth
+                canvas.height = window.innerHeight
+            })
+
+            // 点击放烟花
+            window.addEventListener('click',(event)=>{
+                console.log(event.clientX)
+                console.log(event.clientY)
+                this.addFirework(event.clientX,event.clientY)
+                setTimeout(() => {
+                    this.addFirework(event.clientX,event.clientY)
+                }, 200);
+            })
+        }
+    }
+    addFirework(x,y){
+        const canvas = document.querySelector("canvas");
+        const ctx = canvas.getContext("2d");
+        Fireworks.push(new Firework(x,y,ctx))
+    }
     render() { 
-        return (
-        <canvas>
-            
-        </canvas>
-        );
+        return (<canvas></canvas>);
     }
 }
  
--- src/functions.js
+++ src/functions.js
@@ -51,3 +51,115 @@ export function parsingRemainTime(remainTime){
 
     return parsedRemainTime
 }
+
+
+export class Firework {
+    // 构造器
+    constructor(x,y,ctx){
+        this.x = x
+        this.y = y
+        this.ctx = ctx
+        this.count = Math.round(Math.random() * 15 + 10)
+        this.radius = Math.round(Math.random() * 2 + 10)
+        this.speed = 30
+        this.gravaty = 1
+
+        this.alpha = 1
+        
+        this.points = []
+
+        for (let i = 0; i < this.count; i++) {
+            // let angle = 360 / this.count * i;
+            // 生成角度半径和hsl色相
+            let point = new Point;
+            point.angle = Math.random() * 360;
+            point.radius = Math.round(Math.random() * 5 + 1)
+            point.hue = Math.random() * 360;
+
+            let radians = point.angle * Math.PI / 180;
+
+            // 求出点的xy
+            let arcx = Math.cos(radians) * point.radius
+            let arcy = Math.sin(radians) * point.radius + (this.gravaty * 0.1)
+
+            // 开始绘制
+            this.ctx.beginPath();
+            this.ctx.arc(arcx + this.x, arcy + this.y , 2, Math.PI * 2, false);
+
+            // 填色
+            this.ctx.closePath();
+            let color = `HSL(${point.hue},80%,60%)`
+            console.log(color)
+            this.ctx.fillStyle = color
+            console.log(point.hue)
+            this.ctx.fill();
+
+            this.points.push(point)
+        }
+    }
+
+    // 画每帧的烟花
+    drawFireworks(){
+        for (let index = 0; index < this.points.length; index++) {
+            const element = this.points[index];
+
+            console.log(element)
+            let radians = element.angle * Math.PI / 180;
+
+            // 求出点的xy
+            let arcx = Math.cos(radians) * element.radius
+            let arcy = Math.sin(radians) * element.radius + (this.gravaty * 0.1)
+            
+            // 开始绘制
+            this.ctx.beginPath();
+            this.ctx.arc(arcx + this.x, arcy + this.y , 2, Math.PI * 2, false);
+
+            // 填色
+            this.ctx.closePath();
+            let color = `HSL(${element.hue},80%,60%)`
+            console.log(color)
+            this.ctx.fillStyle = color
+            console.log(element.color)
+            this.ctx.fill();
+
+            // 通过增大半径逐渐将点外移
+            element.radius *= 1 + this.speed / 120
+            this.speed = this.speed * 0.9976
+            this.alpha -= 0.001;
+
+            // 计算y下坠值
+            this.gravaty *= 1.006
+            
+        }
+        
+        // for (let i = 0; i < this.count; i++) {
+        //     let angle = 360 / this.count * i;
+        //     let radians = angle * Math.PI / 180;
+
+        //     // 求出烟花xy
+        //     let arcx = Math.cos(radians) * this.radius
+        //     let arcy = Math.sin(radians) * this.radius + (this.gravaty * 0.1)
+
+        //     this.ctx.beginPath();
+        //     this.ctx.arc(arcx + this.x, arcy + this.y , 2, Math.PI * 2, false);
+
+        //     this.ctx.closePath();
+        //     this.ctx.fillStyle = `rgba(255,0,0,${this.alpha})`
+        //     this.ctx.fill();
+        // }
+        // this.radius *= 1 + this.speed / 120
+        // this.alpha -= 0.019;
+
+        // //计算y下坠值
+        // this.gravaty *= 1.14
+    }
+}
+
+class Point{
+    constructor(radius,hue,speed,angle){
+        this.radius = radius;
+        this.hue = hue;
+        this.speed = speed;
+        this.angle = angle;
+    }
+}
